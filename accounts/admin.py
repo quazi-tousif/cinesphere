75f8cdbeaa6ec1ec744b3967b5f71fefcd6d5bca
quazi-tousif <pritam.sarkar@g.bracu.ac.bd>
2026-06-18 14:08:18 +0600

    Add user profile fields and customize admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "is_staff",
+        "is_superuser",
+        "is_spam",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_staff",
+        "is_superuser",
+        "is_spam",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Profile Information",
+            {
+                "fields": (
+                    "bio",
+                    "date_of_birth",
+                    "profile_picture",
+                    "is_spam",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,32 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    bio = models.TextField(blank=True)
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/",
+        blank=True,
+        null=True
+    )
+
+    is_spam = models.BooleanField(
+        default=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.username
