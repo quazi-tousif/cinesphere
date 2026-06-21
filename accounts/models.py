eb3b6b60bcc6ac3db3b9b38729289e5d35e64984
quazi-tousif <pritam.sarkar@g.bracu.ac.bd>
2026-06-21 13:48:02 +0600

    Add user follow system

--- accounts/models.py
+++ accounts/models.py
@@ -30,3 +30,40 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+    @property
+    def followers_count(self):
+        return self.follower_relationships.count()
+
+
+    @property
+    def following_count(self):
+        return self.following_relationships.count()
+    
+    def follow(self, user):
+        from social.models import Follow
+
+        if self == user:
+            return False
+
+        _, created = Follow.objects.get_or_create(
+            follower=self,
+            following=user,
+        )
+
+        return created
+    
+    def unfollow(self, user):
+        from social.models import Follow
+
+        deleted, _ = Follow.objects.filter(
+            follower=self,
+            following=user,
+        ).delete()
+
+        return deleted > 0
+    
+    def is_following(self, user):
+        return self.following_relationships.filter(
+            following=user
+        ).exists()
