d2edea2dece3e0af9d75b45ab964afd23b6f5aa7
quazi-tousif <pritam.sarkar@g.bracu.ac.bd>
2026-06-21 20:20:11 +0600

    Add follow and unfollow functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import profile_view
+from .views import profile_view, follow_toggle
 
 app_name = "accounts"
 
 urlpatterns = [
     path("<str:username>/", profile_view, name="profile"),
+    path("<str:username>/follow/", follow_toggle, name="follow_toggle"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import User
 
 
 def profile_view(request, username):
     user = get_object_or_404(User, username=username)
 
+    is_following = False
+
+    if request.user.is_authenticated:
+        is_following = request.user.is_following(user)
+
     return render(
         request,
         "accounts/profile.html",
         {
             "profile_user": user,
+            "is_following": is_following,
         },
     )
+
+@login_required
+def follow_toggle(request, username):
+    target = get_object_or_404(User, username=username)
+
+    if request.user == target:
+        return redirect("accounts:profile", username=username)
+
+    if request.user.is_following(target):
+        request.user.unfollow(target)
+    else:
+        request.user.follow(target)
+
+    return redirect("accounts:profile", username=username)
