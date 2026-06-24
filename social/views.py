ef4f5d5d4448e915f528c83501e9d6897f361c81
quazi-tousif <pritam.sarkar@g.bracu.ac.bd>
2026-06-24 15:47:27 +0600

    Implement movie review submission and display

--- social/views.py
+++ social/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
-from .models import Movie
-
+from .forms import ReviewForm
+from .models import Movie, Review
 
 def movie_list(request):
     movies = Movie.objects.all()
@@ -10,4 +10,33 @@ def movie_list(request):
 
 def movie_detail(request, movie_id):
     movie = get_object_or_404(Movie, id=movie_id)
-    return render(request, "social/movie_detail.html", {"movie": movie})
+
+    reviews = Review.objects.filter(movie=movie).select_related("user")
+
+    if request.method == "POST" and request.user.is_authenticated:
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.movie = movie
+            Review.objects.update_or_create(
+                user=request.user,
+                movie=movie,
+                defaults={
+                    "rating": review.rating,
+                    "review_text": review.review_text,
+                    },
+                )
+
+            return redirect("social:movie_detail", movie_id=movie.id)
+    else:
+        form = ReviewForm()
+
+    context = {
+        "movie": movie,
+        "reviews": reviews,
+        "form": form,
+    }
+
+    return render(request, "social/movie_detail.html", context)
